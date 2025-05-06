cd3756e3485d57accfaff45c753de5406aac97a0
EliamHannaUpssitech <eliam.hanna@univ-tlse3.fr>
2025-05-06 13:50:52 +0200

    problemes dus au merge git regles

--- Test_Eliam/IHM.py
+++ Test_Eliam/IHM.py
@@ -20,27 +20,27 @@ def mainIHM():
     Radiobutton(IHM, text="English", variable=selected_value, value="English", command=on_language_change).place(x=750, y=70)
 
     btn_power = Button(IHM, command=exit, bg=None)
-    img_power = PhotoImage(file="images_IHM/power_button.png").subsample(7, 7)
+    img_power = PhotoImage(file="Test_Eliam/images_IHM/power_button.png").subsample(7, 7)
     btn_power.config(image=img_power)
     btn_power.place(x=25, y=25)
 
     btn_manuel = Button(IHM, command=modeManuel, bg=None)
-    img_manuel = PhotoImage(file="images_IHM/M_button.png").subsample(4, 4)
+    img_manuel = PhotoImage(file="Test_Eliam/images_IHM/M_button.png").subsample(4, 4)
     btn_manuel.config(image=img_manuel)
     btn_manuel.place(x=233, y=183)
 
     btn_vocal = Button(IHM, command=modeVocal, bg=None)
-    img_vocal = PhotoImage(file="images_IHM/V_button.png").subsample(4, 4)
+    img_vocal = PhotoImage(file="Test_Eliam/images_IHM/V_button.png").subsample(4, 4)
     btn_vocal.config(image=img_vocal)
     btn_vocal.place(x=233, y=383)
 
     btn_autom = Button(IHM, command=modeAutom, bg=None)
-    img_autom = PhotoImage(file="images_IHM/A_button.png").subsample(4, 4)
+    img_autom = PhotoImage(file="Test_Eliam/images_IHM/A_button.png").subsample(4, 4)
     btn_autom.config(image=img_autom)
     btn_autom.place(x=533, y=183)
 
     btn_image = Button(IHM, command=modeImage, bg=None)
-    img_image = PhotoImage(file="images_IHM/I_button.png").subsample(4, 4)
+    img_image = PhotoImage(file="Test_Eliam/images_IHM/I_button.png").subsample(4, 4)
     btn_image.config(image=img_image)
     btn_image.place(x=533, y=383)
 
@@ -54,32 +54,32 @@ def modeManuel():
     refresh()
 
     btn_retour = Button(IHM, command=mainIHM, bg=None)
-    img_return = PhotoImage(file="images_IHM/return_button.png").subsample(7, 7)
+    img_return = PhotoImage(file="Test_Eliam/images_IHM/return_button.png").subsample(7, 7)
     btn_retour.config(image=img_return)
     btn_retour.place(x=25, y=25)
 
     btn_up = Button(IHM, command=cmd_test, bg=None)
-    img_up = PhotoImage(file="images_IHM/Up_button.png").subsample(5, 5)
+    img_up = PhotoImage(file="Test_Eliam/images_IHM/Up_button.png").subsample(5, 5)
     btn_up.config(image=img_up)
     btn_up.place(x=100+xarrow, y=0+yarrow)
 
     btn_down = Button(IHM, command=cmd_test, bg=None)
-    img_down = PhotoImage(file="images_IHM/Down_button.png").subsample(5, 5)
+    img_down = PhotoImage(file="Test_Eliam/images_IHM/Down_button.png").subsample(5, 5)
     btn_down.config(image=img_down)
     btn_down.place(x=100+xarrow, y=200+yarrow)
 
     btn_left = Button(IHM, command=cmd_test, bg=None)
-    img_left = PhotoImage(file="images_IHM/Left_button.png").subsample(5, 5)
+    img_left = PhotoImage(file="Test_Eliam/images_IHM/Left_button.png").subsample(5, 5)
     btn_left.config(image=img_left)
     btn_left.place(x=0+xarrow, y=100+yarrow)
 
     btn_right = Button(IHM, command=cmd_test, bg=None)
-    img_right = PhotoImage(file="images_IHM/Right_button.png").subsample(5, 5)
+    img_right = PhotoImage(file="Test_Eliam/images_IHM/Right_button.png").subsample(5, 5)
     btn_right.config(image=img_right)
     btn_right.place(x=200+xarrow, y=100+yarrow)
 
     btn_car = Button(IHM, command=cmd_test, bg=None)
-    img_car = PhotoImage(file="images_IHM/car_button.png").subsample(7, 7)
+    img_car = PhotoImage(file="Test_Eliam/images_IHM/car_button.png").subsample(7, 7)
     btn_car.config(image=img_car)
     btn_car.place(x=600, y=230)
 
@@ -138,12 +138,12 @@ def modeVocal():
     refresh()
 
     btn_retour = Button(IHM, command=mainIHM, bg=None)
-    img_return = PhotoImage(file="images_IHM/return_button.png").subsample(7, 7)
+    img_return = PhotoImage(file="Test_Eliam/images_IHM/return_button.png").subsample(7, 7)
     btn_retour.config(image=img_return)
     btn_retour.place(x=25, y=25)
 
     btn_micro = Button(IHM, command=cmd_test, bg=None)
-    img_micro = PhotoImage(file="images_IHM/Micro_button.png").subsample(7, 7)
+    img_micro = PhotoImage(file="Test_Eliam/images_IHM/Micro_button.png").subsample(7, 7)
     btn_micro.config(image=img_micro)
     btn_micro.place(x=200, y=250)
 
@@ -204,7 +204,7 @@ def modeAutom():
     refresh()
 
     btn_retour = Button(IHM, command=mainIHM, bg=None)
-    img_return = PhotoImage(file="images_IHM/return_button.png").subsample(7, 7)
+    img_return = PhotoImage(file="Test_Eliam/images_IHM/return_button.png").subsample(7, 7)
     btn_retour.config(image=img_return)
     btn_retour.place(x=25, y=25)
 
@@ -265,7 +265,7 @@ def modeImage():
     refresh()
 
     btn_retour = Button(IHM, command=mainIHM, bg=None)
-    img_return = PhotoImage(file="images_IHM/return_button.png").subsample(7, 7)
+    img_return = PhotoImage(file="Test_Eliam/images_IHM/return_button.png").subsample(7, 7)
     btn_retour.config(image=img_return)
     btn_retour.place(x=25, y=25)
 
