00c432dc9d5b41240c2ec9bcefb84c0cff1c8604
EliamHannaUpssitech <eliam.hanna@univ-tlse3.fr>
2025-05-07 15:03:20 +0200

    Ajout texte + base manuel manette

--- Test_Eliam/IHM.py
+++ Test_Eliam/IHM.py
@@ -2,42 +2,64 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 from object_details import *
-from CommandeManuelBT_int import *
+from manuel_clavier import *
+from manuel_manette import *
 
 def mainIHM():
 
     refresh()
+
+    text_name = Label(IHM, text="--( MYNSSL )--", font=('broadway' , 25))
+    text_name.place(x=350, y=45)
 
     btn_flag = Button(IHM, command=language_change, bg=None)
     if(langue == 'FR'): img_flag = PhotoImage(file="Test_Eliam/images_IHM/fr_flag.png").subsample(4, 4)
     if(langue == 'EN'): img_flag = PhotoImage(file="Test_Eliam/images_IHM/us_flag.png").subsample(4, 4)
     btn_flag.config(image=img_flag)
     btn_flag.place(x=775, y=25)
+    if(langue=='FR'):   text_langue = Label(IHM, text="Changer la\nlangue")
+    elif(langue=='EN'): text_langue = Label(IHM, text="Change the\nlangage")
+    text_langue.place(x=777, y=97)
 
     btn_power = Button(IHM, command=exit, bg=None)
     img_power = PhotoImage(file="Test_Eliam/images_IHM/power_button.png").subsample(7, 7)
     btn_power.config(image=img_power)
     btn_power.place(x=25, y=25)
+    if(langue=='FR'):   text_power = Label(IHM, text="Eteindre le\nprogramme")
+    elif(langue=='EN'): text_power = Label(IHM, text="Switch off\nthe program")
+    text_power.place(x=29, y=105)
 
     btn_manuel = Button(IHM, command=modeManuel, bg=None)
     img_manuel = PhotoImage(file="Test_Eliam/images_IHM/M_button.png").subsample(4, 4)
     btn_manuel.config(image=img_manuel)
     btn_manuel.place(x=233, y=183)
+    if(langue=='FR'):   text_manuel = Label(IHM, text="Mode Manuel")
+    elif(langue=='EN'): text_manuel = Label(IHM, text="Manual mode")
+    text_manuel.place(x=263, y=163)
 
     btn_vocal = Button(IHM, command=modeVocal, bg=None)
     img_vocal = PhotoImage(file="Test_Eliam/images_IHM/V_button.png").subsample(4, 4)
     btn_vocal.config(image=img_vocal)
     btn_vocal.place(x=233, y=383)
+    if(langue=='FR'):   text_vocal = Label(IHM, text="Mode Vocal")
+    elif(langue=='EN'): text_vocal = Label(IHM, text="Vocal mode")
+    text_vocal.place(x=268, y=363)
 
     btn_autom = Button(IHM, command=modeAutom, bg=None)
     img_autom = PhotoImage(file="Test_Eliam/images_IHM/A_button.png").subsample(4, 4)
     btn_autom.config(image=img_autom)
     btn_autom.place(x=533, y=183)
+    if(langue=='FR'):   text_vocal = Label(IHM, text="Mode Automatisme")
+    elif(langue=='EN'): text_vocal = Label(IHM, text="Automation Mode")
+    text_vocal.place(x=547, y=163)
 
     btn_image = Button(IHM, command=modeImage, bg=None)
     img_image = PhotoImage(file="Test_Eliam/images_IHM/I_button.png").subsample(4, 4)
     btn_image.config(image=img_image)
     btn_image.place(x=533, y=383)
+    if(langue=='FR'):   text_vocal = Label(IHM, text="Mode Image")
+    elif(langue=='EN'): text_vocal = Label(IHM, text="Picture Mode")
+    text_vocal.place(x=562, y=363)
 
     IHM.mainloop()
 
@@ -45,7 +67,7 @@ def mainIHM():
 def modeManuel():
 
     xarrow = 100
-    yarrow = 150
+    yarrow = 250
     
     refresh()
 
@@ -53,6 +75,11 @@ def modeManuel():
     img_return = PhotoImage(file="Test_Eliam/images_IHM/return_button.png").subsample(7, 7)
     btn_retour.config(image=img_return)
     btn_retour.place(x=25, y=25)
+
+    btn_car = Button(IHM, command=cmd_test, bg=None)
+    img_car = PhotoImage(file="Test_Eliam/images_IHM/car_button.png").subsample(8, 8)
+    btn_car.config(image=img_car)
+    btn_car.place(x=87+xarrow, y=-170+yarrow)
 
     btn_up = Button(IHM, command=cmd_test, bg=None)
     img_up = PhotoImage(file="Test_Eliam/images_IHM/Up_button.png").subsample(5, 5)
@@ -77,12 +104,12 @@ def modeManuel():
     btn_clavier = Button(IHM, command=commandeManuel, bg=None)
     img_clavier = PhotoImage(file="Test_Eliam/images_IHM/clavier_button.png").subsample(4, 4)
     btn_clavier.config(image=img_clavier)
-    btn_clavier.place(x=600, y=130)
+    btn_clavier.place(x=600, y=100)
 
     btn_manette = Button(IHM, command=cmd_test, bg=None)
     img_manette = PhotoImage(file="Test_Eliam/images_IHM/manette_button.png").subsample(4, 4)
     btn_manette.config(image=img_manette)
-    btn_manette.place(x=600, y=330)
+    btn_manette.place(x=600, y=360)
 
     IHM.mainloop()
 ##########
@@ -102,8 +129,9 @@ def modeVocal():
     btn_micro.config(image=img_micro)
     btn_micro.place(x=150, y=250)
 
-    text_vocal = Text(IHM, bg=None, height=40, width=60)
+    text_vocal = Text(IHM, bg=None, height=33, width=55)
     text_vocal.place(x=400, y=35)
+    text_vocal.config(state=DISABLED)
 
     IHM.mainloop()
 ##########
@@ -121,12 +149,12 @@ def modeAutom():
     btn_carto = Button(IHM, command=modeCarto, bg=None)
     img_carto = PhotoImage(file="Test_Eliam/images_IHM/carto_button.png").subsample(3, 3)
     btn_carto.config(image=img_carto)
-    btn_carto.place(x=233, y=183)
+    btn_carto.place(x=183, y=186)
 
     btn_trajet = Button(IHM, command=modeTrajet, bg=None)
     img_trajet = PhotoImage(file="Test_Eliam/images_IHM/trajet_button.png").subsample(2, 2)
     btn_trajet.config(image=img_trajet)
-    btn_trajet.place(x=533, y=183)
+    btn_trajet.place(x=533, y=180)
 
     IHM.mainloop()
 
@@ -173,10 +201,11 @@ def modeImage():
 
     cac = carac_obj(image)
 
-    text_image = Text(IHM, bg=None, height=5, width=100)
+    text_image = Text(IHM, bg=None, height=5, width=90)
     text_image.place(x=150, y=25)
     for obj in range(cac[3]):
         text_image.insert(str(obj+1) + ".0", "Objet " + str(obj+1) + " : " + str(cac[0][obj]) + " " + str(cac[1][obj]) + "\n")
+    text_image.config(state=DISABLED)
 
     IHM.mainloop()
 ##########
@@ -202,9 +231,13 @@ def cmd_test():
 # Variables Globales Obligatoires
 langue = "FR"
 
+# Début du programme
 IHM = Tk()
 IHM.geometry('900x600')
 #IHM.configure(bg="gray",relief="raised")
 
 mainIHM()
+
+##
+#
 
